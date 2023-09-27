756cb4c8558ed193993bef212b6ac957492c7386
crewiz <s3salas@gmail.com>
2023-09-27 12:08:17 +0200

    Typos and responsiveness fixes

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -10,8 +10,8 @@ const AboutMe = () => {
   useEffect(() => {
     let index1 = 0;
     let index2 = 0;
-    const text1 = "I am a fullstack javascript developer in training that is passionate about both the Frontend & Backend. I am consistently working on growing and enhancing my skills as developer and project management. I have been described as a persistent solutions-oriented teamplayer with good problemsolving & leadership skills.";
-    const text2 = "Looking for opportunites to sharpen myself as a developer aswell as gain experience working in bigger teams, my current goal is to secure an internship in an enviroment that will allow me to refine the skills i have, and gain new ones.";
+    const text1 = "I am a fullstack javascript developer in training that is passionate about both the Frontend & Backend. I am consistently working on growing and enhancing my skills as a developer and project managing. I have been described as a persistent worker and teamplayer with good problemsolving & leadership skills.";
+    const text2 = "Looking to secure an internship or a job-opportunity in an environment that will allow me to refine the skills i have, and gain new ones. I am also currently learning Typescript as part of my training. I am always for opportunites to sharpen myself as a developer aswell as gain experience in bigger teams. ";
 
     const typing1 = setInterval(() => {
       if(index1 < text1.length) {
@@ -45,15 +45,25 @@ const AboutMe = () => {
   return (
     <section id="aboutMe">
       <div className="content-wrapper">
-      <h2 className={`section-title ${glow === 'in' ? 'glow-text' : glow === 'out' ? 'glow-text-fade-out' : ''}`}>Fullstack Developer</h2>
-    
-        <p className={glow === 'in' ? 'glow-text' : glow === 'out' ? 'glow-text-fade-out' : ''}>{content1}</p>
-        <h3>Currently I am..</h3>
-        <p className={glow === 'in' ? 'glow-text' : glow === 'out' ? 'glow-text-fade-out' : ''}>{content2}</p>
+        
+        
+        <div className="paragraph-container">
+        <h2 className={`section-title ${glow === 'in' ? 'glow-text' : glow === 'out' ? 'glow-text-fade-out' : ''}`}>Fullstack Developer</h2>
+        <div className="inner-container">
+          <p className={glow === 'in' ? 'glow-text' : glow === 'out' ? 'glow-text-fade-out' : ''}>{content1}</p>
+        </div>
+        </div>
+        
+        <div className="paragraph-container">
+        <h2 className={`section-title ${glow === 'in' ? 'glow-text' : glow === 'out' ? 'glow-text-fade-out' : ''}`}>Currently i am...</h2>
+        <div className="inner-container">
+
+          <p className={glow === 'in' ? 'glow-text' : glow === 'out' ? 'glow-text-fade-out' : ''}>{content2}</p>
+        </div>
+      </div>
       </div>
     </section>
   );
-  
 };
 
 export default AboutMe;
